7059feb8c0bc079c24d85bfff20f5995c36a7524
iluvcheese <lukeskywalkervanderwant@gmail.com>
2024-06-26 19:02:23 +0200

    Add files via upload

---
+++ turtles/yep.py
@@ -0,0 +1,21 @@
+alldaturtlesindaneighborhood = {1,2, 3, 4, 5, 6, 7, 8, 10, 1, 2, 3, 4, 5, 6, 7, 8, 10}
+
+print(alldaturtlesindaneighborhood)
+
+if 7 in alldaturtlesindaneighborhood:
+    print("cheese")
+
+alldaturtlesindaneighborhood.add("9 but he got eaten by 7 cuz like seven ate nine")
+
+def a():
+    print(alldaturtlesindaneighborhood)
+
+a()
+
+""" 12345678in here10 """
+
+alldaturtlesindaneighborhood.remove("9 but he got eaten by 7 cuz like seven ate nine")
+
+a()
+
+alldaturtlesindaneighborhood.discard("oioioi")
